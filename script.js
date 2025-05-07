b0518514e5ca82fa412f0d117e288ca8281ca8ba
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-05-07 10:32:49 +0700

    chore: using different query selector

--- script.js
+++ script.js
@@ -3,13 +3,9 @@ let celcius = Number(temp.textContent);
 const fahrenheit = (9/5 * celcius) + 32;
 const kelvin = celcius + 273.15;
 const reamur = celcius + (4/5);
-console.log(fahrenheit);
-console.log(kelvin);
-console.log(reamur);
 
-const fahText = document.querySelector(`body>p:nth-child(3)>span`);
+const fahText = document.querySelector(`h1+p+p:nth-child(3)>span`);
 fahText.textContent = fahrenheit;
-console.log(fahText);
 const kelText= document.querySelector('body>p:nth-child(4)>span');
 kelText.textContent = kelvin;
 const reText= document.querySelector('body>p:nth-child(5)>span');

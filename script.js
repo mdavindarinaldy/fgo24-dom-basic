d3b71bf5fd4f466b763d85742e536dafc21a7a3a
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-05-07 10:53:06 +0700

    fix: fix reamur formula

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const temp = document.querySelector('h1+p:nth-child(2)>span');
 let celcius = Number(temp.textContent);
 const fahrenheit = (9/5 * celcius) + 32;
 const kelvin = celcius + 273.15;
-const reamur = celcius + (4/5);
+const reamur = celcius * (4/5);
 
 const fahText = document.querySelector(`h1+p+p:nth-child(3)>span`);
 fahText.textContent = fahrenheit;
